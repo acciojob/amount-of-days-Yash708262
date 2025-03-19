4523e80637e8ea7d1fd348d1a9bae0c73bdfe061
Yash708262 <yash708262@gmail.com>
2025-03-19 12:25:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function daysOfAYear(year) {
+	 if (year < 1 || year > 9999) {
+        return "Invalid input: Year must be between 1 and 9999.";
+    }
 	if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
 		return 366;
 	} else {
